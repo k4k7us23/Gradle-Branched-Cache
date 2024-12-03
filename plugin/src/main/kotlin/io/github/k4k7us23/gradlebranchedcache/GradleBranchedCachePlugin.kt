dc6545a8381abc79963d1902a0e9821d3db1e538
k4ktus23 <pavel.krylov.03@gmail.com>
2024-12-03 20:13:11 +0300

    Added GradleBranchedCachePluginExtension.branchNameOverride

--- plugin/src/main/kotlin/io/github/k4k7us23/gradlebranchedcache/GradleBranchedCachePlugin.kt
+++ plugin/src/main/kotlin/io/github/k4k7us23/gradlebranchedcache/GradleBranchedCachePlugin.kt
@@ -1,6 +1,7 @@
 package io.github.k4k7us23.gradlebranchedcache
 
 import io.github.k4k7us23.gradlebranchedcache.vcs.BranchCacheKeyProvider
+import io.github.k4k7us23.gradlebranchedcache.vcs.BranchNameProvider
 import io.github.k4k7us23.gradlebranchedcache.vcs.GitVCS
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
@@ -9,25 +10,29 @@ import java.net.URI
 
 class GradleBranchedCachePlugin : Plugin<Settings> {
 
-    private val branchKeyProvider = BranchCacheKeyProvider(
-        vcs = GitVCS()
-    )
-
     private lateinit var extension: GradleBranchedCachePluginExtension
 
     override fun apply(settings: Settings) {
         extension =
             settings.extensions.create("branchedRemoteBuildCache", GradleBranchedCachePluginExtension::class.java)
 
+        val branchCacheKeyProvider = BranchCacheKeyProvider(
+            branchNameProvider = BranchNameProvider(
+                vcs = GitVCS(),
+                extension = extension,
+            )
+        )
+
         settings.gradle.settingsEvaluated {
             settings.buildCache { buildCache ->
                 buildCache.remote(HttpBuildCache::class.java) { remote ->
-                    remote.url = getFinalRemoteUrl()
+                    remote.url = getFinalRemoteUrl(branchCacheKeyProvider)
                     extension.push?.let(remote::setPush)
 
                     extension.enabled?.let(remote::setEnabled)
                     extension.allowUntrustedServer?.let(remote::setAllowUntrustedServer)
                     extension.allowInsecureProtocol?.let(remote::setAllowInsecureProtocol)
+                    extension.useExpectContinue?.let(remote::setUseExpectContinue)
 
                     extension.credentials.let { extensionCredentials ->
                         val remoteCredentials = remote.credentials
@@ -41,9 +46,9 @@ class GradleBranchedCachePlugin : Plugin<Settings> {
 
     // TODO check base url is not empty
     // TODO support no slash at the end
-    private fun getFinalRemoteUrl(): URI {
+    private fun getFinalRemoteUrl(branchCacheKeyProvider: BranchCacheKeyProvider): URI {
         val baseUrl = URI(requireNotNull(extension.baseUrl))
-        return baseUrl.resolve(branchKeyProvider.getKey())
+        return baseUrl.resolve(branchCacheKeyProvider.getKey())
     }
 
 }
